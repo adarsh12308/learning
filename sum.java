094b634cab0b6488f4592a71db5fd3c4723c876a
Adarsh Singh <aaddaarrsshh96@gmail.com>
2021-11-09 11:24:14 +0530

    adding sum

--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ class sum
     public static void main(String[] args){
         int a=5;
         int b=6;
-        System.out.println("Sum of 2 number ="+a+b);
+        System.out.println("Sum of 2 number ="+(a+b));
     }
 }
